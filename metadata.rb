0835b8c144eb7d04cf3dc3760f63d48efebd3aee
Caleb Severn <calsev@gmail.com>
2022-01-13 09:31:30 -0800

    Make dependencies optimistic

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,10 @@ maintainer       'Dang H. Nguyen'
 maintainer_email 'haidangwa@gmail.com'
 license          'Apache-2.0'
 description      'Installs Terraform (terraform.io)'
-version          '3.0.1'
+version          '3.0.2'
 
-depends 'ark', '~> 5.0'
-depends 'gpg', '~> 1.1'
+depends 'ark', '>= 5.0'
+depends 'gpg', '>= 1.1'
 
 supports 'centos', '> 7.0'
 supports 'debian', '> 7.0'
